753a593366adc5b270934398c4264262b0519ba0
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2023-07-25 08:40:29 -0300

    Alteraçoes

--- projeto-random/script.js
+++ projeto-random/script.js
@@ -6,9 +6,19 @@ function clicknumber() {
     const min = Math.ceil(document.querySelector('.input1').value)
     const max = Math.floor(document.querySelector('.input2').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max){
+        alert('O valor minimo tem que ser  Menor  que ser valor máximo')
+    } else {
+    
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
+    }
+
+
+  
+
+    
 }
 
 
